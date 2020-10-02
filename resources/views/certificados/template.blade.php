82cdf7f33a34ba3e2fcf184a9dbbb497fda4101f
Robson Fernando Duda <robsonferduda@gmail.com>
2020-10-02 09:56:27 -0300

    Alterações

--- resources/views/certificados/template.blade.php
+++ resources/views/certificados/template.blade.php
@@ -1,13 +1,14 @@
 <html>
     <head>
         <style>
-            body { background-image: url(http://localhost/certificados/uploads/modelos/certf_diretor_coordenador_limpo.jpg); background-position: bottom right; background-repeat: no-repeat; }
+            body { background-image: url({{ ENV('ASSET_URL').'/img/modelos/certf_diretor_coordenador_limpo.jpg' }}); background-position: bottom right; background-repeat: no-repeat; }
             .corpo { padding-top: 200px; }
         </style>
     </head>
     <body>
         <div class="corpo">
             {!! $certificado->de_texto_certificado !!}
+            {{ ENV('ASSET_URL').'/uploads/modelos/certf_diretor_coordenador_limpo.jpg' }}
         </div>
     </body>
 </html>
