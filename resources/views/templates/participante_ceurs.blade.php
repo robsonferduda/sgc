9086100696b5b2ed3556903f3b78360a128449d4
Robson Fernando Duda <robsonferduda@gmail.com>
2022-01-04 15:54:14 -0300

    Template CEURS

--- resources/views/templates/participante_ceurs.blade.php
+++ resources/views/templates/participante_ceurs.blade.php
@@ -34,14 +34,15 @@
             <h2>Certificamos que</h2>
             <h1>{{ $certificado->participante->ds_nome_par }}</h1>
             <p>
-                atuou como Participante da <strong>CAPACITAÇÃO EM MUNICIPALIZAÇÃO DA AGENDA 2030</strong> realizado no período de <strong>01/10/2021</strong> a <strong>31/10/2021</strong> com carga horária de <strong>10</strong> horas.
+                atuou como Participante da <strong>CAPACITAÇÃO EM MUNICIPALIZAÇÃO DA AGENDA 2030</strong> 
+            </p>
+            <p>
+                realizado no período de <strong>01/10/2021</strong> a <strong>31/10/2021</strong> com carga horária de <strong>10</strong> horas.
             </p>
             <p>Esta atividade está amparada pelo Projeto de Extensão intitulado: Curso Introdutório à Municipalização da Agenda 2030</p>
 
             <h3>Tópicos Abordados</h3>
-            <p>Introdução à Agenda 2030</p>
-            <p>Gestão de Cidades</p>
-            <p>Municipalização da Agenda 2030</p>
+            <p>Introdução à Agenda 2030; Gestão de Cidades; Municipalização da Agenda 2030.</p>
         </div>
     </body>
 </html>
