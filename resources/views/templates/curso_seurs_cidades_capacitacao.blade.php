46fd2f1e15cfa1a820fec13b6190e50579ec8afd
Robson Fernando Duda <robsonferduda@gmail.com>
2022-11-21 20:35:17 -0300

    Assinatura

--- resources/views/templates/curso_seurs_cidades_capacitacao.blade.php
+++ resources/views/templates/curso_seurs_cidades_capacitacao.blade.php
@@ -45,7 +45,6 @@
             <p>Florianópolis, 15 de novembro de 2022.</p>
             <p></p>
             <p></p>
-            <img style="width: 20%;" src="{{ ENV('ASSET_URL').'/img/fundos/assinatura.jpg' }}" alt="Assinatura">
         </div>
     </body>
 </html>
