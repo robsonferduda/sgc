07623bc822dc1b76928d185a33cb488986ca613e
Robson Fernando Duda <robsonferduda@gmail.com>
2022-11-22 20:58:42 -0300

    Certificado Presencial 2022

---
+++ resources/views/templates/participante_ciki_2022.blade.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <style>
+            body { background-image: url({{ ENV('ASSET_URL').'/img/fundos/ciki_2022.png' }}); background-position: bottom right; background-repeat: no-repeat; }
+            .corpo { text-align: center; padding-top: 180px; min-height: 500px; }
+            .corpo p { font-size: 20px; }
+            .corpo h1 { padding-bottom: 14px; }
+            .corpo h2 { padding-bottom: 14px; }
+
+            .rodape { text-align: center; }
+            .rodape p {font-size: 12px; }
+
+            @page {
+                margin: 10%;
+                margin-header: 3mm; 
+	            margin-footer: 3mm; 
+                background-image: url({{ ENV('ASSET_URL').'/img/fundos/ciki_2022.png' }}); background-position: bottom right; background-repeat: no-repeat;
+
+                header: page-header;
+		        footer: page-footer;
+            }
+
+            @page-footer {
+                font-size: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <htmlpagefooter name="page-footer">
+            <p style="font-size: 13px; text-align: center;">The authenticity of this document must be verified in the url https://certificado.studiokem.ufsc.br/certificados/validacao/{{ $certificado->ds_hash_cer }}<p>
+        </htmlpagefooter>
+
+        <div class="corpo">
+            <h2>Certificate of Participation</h2>
+            <h1>{{ $certificado->participante->ds_nome_par }}</h1>
+            <p>
+                Was present and participated in the XII International Congress of Knowledge and Innovation - CiKi 2022, held in Monterrey, Mexico, on November 7-9, 2022.
+            </p>
+        </div>
+    </body>
+</html>
