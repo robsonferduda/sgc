0c13f7444e9b45eb814259405e4380f011ff8c03
Robson Fernando Duda <robsonferduda@gmail.com>
2022-12-07 10:13:22 -0300

    Certificados CEURS

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -255,7 +255,7 @@ class CertificadoController extends Controller
         if($dados){
 
             $dados->increment('nu_total_impressoes_cer');
-            $equipe = str_replace(",",", ",$certificado->metadados->where('label_metadado_cem','#autores')->first()->valor_metadado_cem);
+            $equipe = str_replace(",",", ",$dados->metadados->where('label_metadado_cem','#autores')->first()->valor_metadado_cem);
             $equipe = strpos(",", $equipe);
 
             $nome_arquivo = 'certificado_'.$tipo.'_'.$dados->participante->ds_email_par.'.pdf';
