544a78c041e3f9c188691edb1e0ca9941d2eb34b
Robson Fernando Duda <robsonferduda@gmail.com>
2023-11-30 22:47:18 -0300

    Notificação

---
+++ resources/views/email/notificacao_workshop.blade.php
@@ -0,0 +1,19 @@
+Olá <strong>{{ $name }}</strong>,
+<p>
+    Informamos que já está disponível o seu certificado de participação no 13º Workshop CHIS, o Workshop Conexão - Praça XV ao Mar.
+</p>
+<p>Para acessar o seu certificado, por favor acesse o seguinte endereço:</p>
+<p><a href="{{ ENV('APP_URL') }}/certificados/gerar/{{ $hash }}">{{ ENV('APP_URL') }}/certificados/gerar/{{ $hash }}</a></p>
+<p>Para visualizar todos os certificados emitidos com seu email acesse:</p>
+<p><a href="{{ ENV('APP_URL') }}/certificados/segunda-via">{{ ENV('APP_URL') }}/certificados/segunda-via</a></p>
+<p>
+    Convidamos você a participar da nossa pesquisa de avaliação do Workshop. 
+    Sua opinião é de extrema importância para podermos aprimorar a metodologia utilizada em eventos semelhantes. As perguntas são objetivas, com espaço para
+    outras contribuições por escrito, caso você deseje fazê-las.
+</p>
+<p>
+    Para participar da nossa pesquisa de avaliação, é só clicar no link:
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFBjjUkl-4PvHmZwXq4MrMWgHrhqT42xElFW_gxaDVs87hmQ/viewform">Avaliação Workshop</a>
+</p>
+<p>O LabCHIS agradece sua participação em mais uma oficina de trabalho.</p>
+<p style="color: red;"><strong>ATENÇÃO</strong>: Esta mensagem foi enviada automaticamente. Por favor, não responda a esta mensagem.</p>
