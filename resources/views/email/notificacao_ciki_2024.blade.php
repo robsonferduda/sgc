7e38b1325a103068297ef400286cb21d4415207b
Robson Fernando Duda <robsonferduda@gmail.com>
2024-12-04 11:54:38 -0300

    Certificado

--- resources/views/email/notificacao_ciki_2024.blade.php
+++ resources/views/email/notificacao_ciki_2024.blade.php
@@ -1,12 +1,9 @@
-Dear <strong>{{ $name }}</strong>,
-<p>
-    We inform you that your {{ $tipo }} in the joint edition of the 14th International Congress on Knowledge and Innovation (CiKi2024) for issuance and validation in our system.
-</p>
-<p>To issue your certificate, please access the following address:</p>
+Prezado (a) <strong>{{ $name }}</strong>,
+<p>Informamos que seu certificado de {{ $tipo }} no XII Congreso Internacional de Conocimiento e Innovación - ciKi está disponível para emissão e validação em nosso sistema.</p>
+<p>Para emitir seu certificado, acesse o seguinte endereço:</p>
 <p><a href="{{ ENV('APP_URL') }}/certificados/gerar/{{ $hash }}">{{ ENV('APP_URL') }}/certificados/gerar/{{ $hash }}</a></p>
-<p>To view all certificates issued with your email, go to:</p>
+<p>Para visualizar todos os certificados emitidos com seu email acesse:</p>
 <p><a href="{{ ENV('APP_URL') }}/certificados/segunda-via">{{ ENV('APP_URL') }}/certificados/segunda-via</a></p>
-<p>Yours sincerely,</p>
-<p>Organizing committee</p>
-<p>14th International Congress on Knowledge and Innovation (CiKi2024)</p>
-<p style="color: red;"><strong>ATTENTION</strong>: This message was sent automatically. Please do not reply to this message.</p>
+<p>Atenciosamente,</p>
+<p>Comissão Organizadora do Congresso Internacional de Conhecimento e Inovação 2022</p>
+<p style="color: red;"><strong>ATENÇÃO</strong>: Esta mensagem foi enviada automaticamente. Por favor, não responda a esta mensagem.</p>
